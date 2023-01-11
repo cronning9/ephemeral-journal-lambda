ac1f7e02fe14a5d9a53b68232f9277503bf36d50
Chris Ronning <christopher.a.ronning@gmail.com>
2023-01-11 17:00:17 -0500

    wip: convert buffer response to text

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 
 exports.handler = async (event, context) => {
-	console.log('Received event:', JSON.stringify(event, null, 2));
+	// console.log('Received event:', JSON.stringify(event, null, 2));
 
 	// Get the object from the event and show its content type
 	const bucket = event.Records[0].s3.bucket.name;
@@ -16,9 +16,8 @@ exports.handler = async (event, context) => {
 		Key: key,
 	};
 	try {
-		const { ContentType } = await s3.getObject(params).promise();
-		console.log('CONTENT TYPE:', ContentType);
-		return ContentType;
+		const Object = await s3.getObject(params).promise();
+		console.log(Object)
 	} catch (err) {
 		console.log(err);
 		const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
