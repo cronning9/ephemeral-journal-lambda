0608b2fc9e408b5019ebe30888b7edd05b9872d1
Chris Ronning <christopher.a.ronning@gmail.com>
2023-01-24 19:01:35 -0500

    log body content

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('Loading function sir!!');
-
 const aws = require('aws-sdk');
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
@@ -8,7 +6,6 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 exports.handler = async (event, context) => {
 	// console.log('Received event:', JSON.stringify(event, null, 2));
 
-	// Get the object from the event and show its content type
 	const bucket = event.Records[0].s3.bucket.name;
 	const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 	const params = {
@@ -16,12 +13,14 @@ exports.handler = async (event, context) => {
 		Key: key,
 	};
 	try {
-		const Object = await s3.getObject(params).promise();
-		console.log(Object)
+		const res = await s3.getObject(params).promise();
+		const body = res.Body.toString();
+		console.log(body);
+		// body.split('\n').forEach(str => console.log(str));
 	} catch (err) {
-		console.log(err);
+		console.error(err);
 		const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-		console.log(message);
+		console.error(message);
 		throw new Error(message);
 	}
 };
