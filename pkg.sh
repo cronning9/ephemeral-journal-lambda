d07824330f023c59dd7b08064ea6102e67359968
Chris Ronning <christopher.a.ronning@gmail.com>
2023-02-03 18:14:48 -0500

    update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 rm ../ephemeral-journal-lambda.zip
-yarn && zip -r ../ephemeral-journal-lambda.zip event.json index.js package.json README.md yarn.lock src
+yarn && zip -r ../ephemeral-journal-lambda.zip events src index.js package.json README.md yarn.lock
