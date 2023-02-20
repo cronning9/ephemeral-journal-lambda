e95cd60ff1aa27a28d508081a4e841ae3e9f6b6b
Chris Ronning <christopher.a.ronning@gmail.com>
2023-02-20 16:55:12 -0500

    fix event processor to handle API Gateway and sam local events

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ We deploy via GitHub Actions. See [Pipeline Configuration](.github/workflows//sa
 - `aws cloudformation validate-template --template-body file://template.yaml`
 - `yarn start:local`
 - `yarn build` _DEPRECATED_ - Bundles up all the relevant files into a .zip, for upload to AWS Lambda console. _This will break the lambda application, which was created via the GitHub Actions. If you use this method, CD will break until you delete the entire application manually from the AWS Console.
+
+### To test with API Gateway locally:
+```
+# run API Gateway process in one terminal window
+sam local start-api
+
+# CURL in separate terminal window
+curl -X POST http://127.0.0.1:3000/Entry/Read -H "Content-Type: application/json" -d '{"requestType": "readFile", "bucketName": "ephemeral-journal-bkt", "objectKey": "test.txt" }' --verbose
+```
--- src/events.js
+++ src/events.js
@@ -5,16 +5,17 @@ const eventTypes = {
 };
 
 async function dispatchEvent(event) {
-	const requestType = event.body.requestType;
-	console.log('request type: ' + requestType);
-	console.log('bucketName: ' + event.body.bucketName)
-	console.log('objectKey: ' + event.body.objectKey)
+	// this handles a weird discrepancy between API Gateway requests and local invocations.
+	// API gateway requests come in as strings that must be parsed, whereas local invocations 
+	// using api-event.json comes in as a pre-parsed object
+	const body = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+	const requestType = body.requestType;
 
 	const func = eventTypes[requestType];
-	const res = func ? await func(event.body) : { 
+	const res = func ? await func(body) : { 
 		statusCode: 400,
 		body: {
-			requestBody: event.body
+			requestBody: body
 		}
 	};
 	return res;
