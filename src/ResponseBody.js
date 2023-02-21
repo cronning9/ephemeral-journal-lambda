e2b235158c8efe2f7047e21a19c53cc53afaaa31
Chris Ronning <christopher.a.ronning@gmail.com>
2023-02-21 13:03:47 -0500

    add isBase64Encoded field
    hopefully this will make the lambda response conform to API gateway standards. See https://aws.amazon.com/premiumsupport/knowledge-center/malformed-502-api-gateway/

--- src/ResponseBody.js
+++ src/ResponseBody.js
@@ -3,10 +3,11 @@ class ResponseBody {
 		'Content-Type': 'application/json'
 	};
 
-	constructor(body, headers={}, statusCode=200) {
+	constructor(body, headers={}, statusCode=200, isBase64Encoded=false) {
 		this.body = body;
 		this.headers = { ...this.headers, ...headers };
 		this.statusCode = statusCode;
+		this.isBase64Encoded = isBase64Encoded;
 	}
 }
 
