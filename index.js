150c57f297c0075ddec2015782b56ca5c98f4a18
Chris Ronning <christopher.a.ronning@gmail.com>
2023-03-06 10:36:58 -0500

    update route handler for Entry/Read

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ResponseBody } = require('./src/ResponseBody');
 const { dispatchEvent }= require('./src/events');
 
 
-exports.handler = async (event, context) => {
+exports.readFile = async (event, context) => {
 	// console.log('Received event:', JSON.stringify(event, null, 2));
 	const res = await dispatchEvent(event);
 	const response = new ResponseBody(res.body, {}, res.statusCode);
